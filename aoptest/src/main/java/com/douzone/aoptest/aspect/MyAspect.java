11e6667ba6f6e53ab100cd2d6429b661ec2abed8
seoin1102 <im_in_@naver.com>
2022-06-10 11:06:52 +0900

    AOP TEST -finish

---
+++ aoptest/src/main/java/com/douzone/aoptest/aspect/MyAspect.java
@@ -0,0 +1,15 @@
+package com.douzone.aoptest.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyAspect {
+	
+	@Before("execution(public com.douzone.aoptest.vo.ProductVo com.douzone.aoptest.service.ProductService.find(String))")
+	public void beforeAdvice() {
+		System.out.println("---Before Advice---");
+	}
+}
---
+++ aoptest/src/main/java/com/douzone/aoptest/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.douzone.aoptest.service;
+
+import org.springframework.stereotype.Service;
+
+import com.douzone.aoptest.vo.ProductVo;
+
+@Service
+public class ProductService {
+
+	public ProductVo find(String name) {
+		return new ProductVo(name);
+	}
+	
+}
